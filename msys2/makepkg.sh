a56a9ec1d85cd565ffcdb597675bb63bfe640754
Tristan Heaven <tristan@tristanheaven.net>
2021-10-17 22:18:25 +0100

    Set MINGW_ARCH list for makepkg-mingw

--- msys2/makepkg.sh
+++ msys2/makepkg.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"/..
 git archive --prefix gtkhash/ -o msys2/mingw-w64-gtkhash/gtkhash.tar.gz HEAD
 
 cd msys2/mingw-w64-gtkhash
-PKGEXT='.pkg.tar.xz' exec makepkg-mingw \
+MINGW_ARCH="mingw64 mingw32" PKGEXT='.pkg.tar.xz' exec makepkg-mingw \
 	--noconfirm \
 	--noprogressbar \
 	--syncdeps \
