c216aed8485094c7194c886c103bd5d685b75027
Tristan Heaven <tristan@tristanheaven.net>
2022-01-01 20:40:01 +0000

    Fix meson install on systems without bash

--- data/icons/rename_icons.sh
+++ data/icons/rename_icons.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 mv_dir="$1"
 file_in="$2"
 file_out="$3"
 
-mv "$MESON_INSTALL_DESTDIR_PREFIX/$mv_dir/$file_in" \
+exec mv -- "$MESON_INSTALL_DESTDIR_PREFIX/$mv_dir/$file_in" \
    "$MESON_INSTALL_DESTDIR_PREFIX/$mv_dir/$file_out"
